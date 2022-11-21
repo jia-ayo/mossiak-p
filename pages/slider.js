ecf2b85991cef5a77f7214f874159b1cdf3c9b4c
jia-ayo <milkjia11@gmail.com>
2022-11-21 05:33:04 +0100

    power point

--- pages/slider.js
+++ pages/slider.js
@@ -1,20 +1,21 @@
 // Import Swiper React components
+import Link from 'next/link';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, {Autoplay} from 'swiper';
 import {Scrollbar} from 'swiper';
 import Image from 'next/image';
-import DocViewer from "react-doc-viewer";
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/scrollbar';
 
  
 export default function Slider() {
+
     SwiperCore.use([Autoplay]);
 
   return (
     <div className='mx-10 md:mx-20 lg:mx-30'>
-    <div className='shadow-lg text-center rounded-xl p-10 my-10 lg:flex lg:justify-evenly bg-slate-300 '>
+    <div className='shadow-lg text-center rounded-xl  my-10 lg:flex lg:justify-center bg-slate-300 '>
     <Swiper
       modules={[Scrollbar]}
       slidesPerView={1}
@@ -24,10 +25,10 @@ export default function Slider() {
         delay: 5000
       }}
     >
-      <SwiperSlide className='flex justify-center'>
+      <SwiperSlide className='flex justify-center pt-3'>
         <div >
-            <div className='relative mx-auto w-40 h-40 lg:mx-auto lg:mt-20 '>
-                   <Image className='lg:pr-1 lg:w-250 lg:h-250'  src="/mos3.png" alt="brand image" width={150} height={150}/>
+            <div className='relative mx-auto lg:mx-auto lg:mt-20 '>
+            <iframe src="https://onedrive.live.com/embed?cid=BBF5E1DD97802BDD&amp;resid=BBF5E1DD97802BDD%21410&amp;authkey=ABmssU5Yg5sSzl0&amp;em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1" width="375px" height="250px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
             </div>
             <div>
                 
