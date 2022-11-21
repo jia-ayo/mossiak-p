c48611cf0428bbb8ac67025a82a37eb6d494fdcd
jia-ayo <milkjia11@gmail.com>
2022-11-21 18:19:23 +0100

    second slide

--- pages/slider.js
+++ pages/slider.js
@@ -15,7 +15,7 @@ export default function Slider() {
 
   return (
     <div className='mx-10 md:mx-20 lg:mx-30'>
-    <div className='shadow-lg text-center rounded-xl  my-10 lg:flex lg:justify-center bg-slate-300 '>
+    <div className=' '>
     <Swiper
       modules={[Scrollbar]}
       slidesPerView={1}
@@ -25,7 +25,7 @@ export default function Slider() {
         delay: 5000
       }}
     >
-      <SwiperSlide className='flex justify-center pt-3'>
+      <SwiperSlide className='flex justify-center pt-3 '>
         <div >
             <div className='relative mx-auto lg:mx-auto lg:mt-20 '>
             <iframe src="https://onedrive.live.com/embed?cid=BBF5E1DD97802BDD&amp;resid=BBF5E1DD97802BDD%21410&amp;authkey=ABmssU5Yg5sSzl0&amp;em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1" width="375px" height="250px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
@@ -35,8 +35,16 @@ export default function Slider() {
             </div>
         </div>
       </SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide className='flex justify-center pt-3'>
+      <div >
+            <div className='relative mx-auto lg:mx-auto lg:mt-20 '>
+            <iframe src="https://onedrive.live.com/embed?cid=BBF5E1DD97802BDD&amp;resid=BBF5E1DD97802BDD%21415&amp;authkey=ACsRrrEW8OH4f4g&amp;em=2&amp;wdAr=1.7777777777777777" width="375px" height="250px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>           </div>
+            <div>
+                
+            </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className='flex justify-center pt-3'>Slide 3</SwiperSlide>
 
       ...
     </Swiper></div></div>
