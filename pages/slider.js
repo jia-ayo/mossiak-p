5e21bfabc47afeb6a3f10cd0574a9c5ebe898d24
jia-ayo <milkjia11@gmail.com>
2022-11-22 04:20:52 +0100

    slide component

--- pages/slider.js
+++ pages/slider.js
@@ -8,6 +8,25 @@ import Image from 'next/image';
 import 'swiper/css';
 import 'swiper/css/scrollbar';
 
+function Slide(props){
+  return(
+    
+    <div className='shadow-lg text-center rounded-xl lg:flex lg:justify-evenly lg:bg-slate-300 md:bg-slate-300'>
+          <div className='flex justify-center pt-10 pb-5 lg:px-5'>
+          {props.presentation}
+          </div>
+          <div className='px-10 lg:mt-10 lg:mx-30 lg:justify-center'>
+              <h1 className='text-xl md:text-2xl lg:text-3xl' >{props.Topic}</h1>
+              <p  className='text-md py-5 leading-8 text-gray-800 md:text-l lg:text-xl max-w-xl mx-auto dark:text-slate-100'>
+                {props.detail}
+              </p>
+          </div>
+      </div>
+    
+
+  )
+}
+
  
 export default function Slider() {
 
@@ -24,63 +43,51 @@ export default function Slider() {
       autoplay= {{
         delay: 5000
       }}
-    >
-      <SwiperSlide >
-      <div className='shadow-lg text-center rounded-xl lg:flex lg:justify-evenly bg-slate-300 '>
-            <div className='flex justify-center pt-10 pb-5 lg:px-5'>
-            <iframe src="https://onedrive.live.com/embed?cid=BBF5E1DD97802BDD&amp;resid=BBF5E1DD97802BDD%21410&amp;authkey=ABmssU5Yg5sSzl0&amp;em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1" width="375px" height="250px" frameborder="0">This is an embedded 
+    > 
+    <SwiperSlide >
+      <Slide 
+          presentation = {<iframe src="https://onedrive.live.com/embed?cid=BBF5E1DD97802BDD&amp;resid=BBF5E1DD97802BDD%21410&amp;authkey=ABmssU5Yg5sSzl0&amp;em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1" width="375px" height="250px" frameborder="0">This is an embedded 
             <a target="_blank" rel="noreferrer" href="https://office.com">Microsoft Office</a> presentation, powered by 
-            <a target="_blank" rel="noreferrer"  href="https://office.com/webapps">Office</a>.</iframe>
-            </div>
-            <div className='px-10 lg:mt-10 lg:mx-30 lg:justify-center'>
-                <h1 className='text-xl md:text-2xl lg:text-3xl' >Architectural Design</h1>
-                <p  className='text-md py-5 leading-8 text-gray-800 md:text-l lg:text-xl max-w-xl mx-auto dark:text-slate-100'>
-                  Architectural Design PowerPoint Template 
+            <a target="_blank" rel="noreferrer"  href="https://office.com/webapps">Office</a>.</iframe>}
+              Topic="Architectural Design"
+              detail="                  Architectural Design PowerPoint Template 
                   Showing the beauty of your finished work or renderings has just gotten easier.  
                   Display the interiors and the exteriors of your masterpiece using a modern style
-                  PowerPoint template guaranteed to give the feel of luxury and homeliness in one.
-                </p>
-            </div>
-        </div>
+                  PowerPoint template guaranteed to give the feel of luxury and homeliness in one."
+      />
       </SwiperSlide>
       <SwiperSlide >
-      <div className='shadow-lg text-center rounded-xl lg:flex lg:justify-evenly bg-slate-300 '>
-            <div className='flex justify-center pt-10 pb-5 lg:px-5'>
-               <iframe src="https://onedrive.live.com/embed?cid=BBF5E1DD97802BDD&amp;resid=BBF5E1DD97802BDD%21415&amp;authkey=ACsRrrEW8OH4f4g&amp;em=2&amp;wdAr=1.7777777777777777" width="375px" height="250px" frameborder="0">This is an embedded 
+      <Slide
+        presentation={
+          <iframe src="https://onedrive.live.com/embed?cid=BBF5E1DD97802BDD&amp;resid=BBF5E1DD97802BDD%21415&amp;authkey=ACsRrrEW8OH4f4g&amp;em=2&amp;wdAr=1.7777777777777777" width="375px" height="250px" frameborder="0">This is an embedded 
                <a target="_blank" rel="noreferrer" href="https://office.com">Microsoft Office</a> presentation, powered by 
-               <a target="_blank" rel="noreferrer" href="https://office.com/webapps">Office</a>.</iframe>           
-            </div>
-            <div className='px-10 lg:mt-10 lg:mx-30 lg:justify-center'>
-                <h1 className='text-xl md:text-2xl lg:text-3xl' >Mental Health</h1>
-                <p  className='text-md py-5 leading-8 text-gray-800 md:text-l lg:text-xl max-w-xl mx-auto dark:text-slate-100'>
+               <a target="_blank" rel="noreferrer" href="https://office.com/webapps">Office</a>.</iframe> 
+        }
+        Topic="Mental Health"
+        detail="
                 The Mental Health PowerPoint template is a simple eye-friendly
                  template that makes use of soft colours and fonts to pass on the message 
                  of safety, trust, and comfort. 
                  This could be used for a lecture or an awareness talk.
-                </p>
-            </div>
-        </div>
+        "
+      />
       </SwiperSlide>
-      <SwiperSlide>
-      <div className='shadow-lg text-center rounded-xl lg:flex lg:justify-evenly bg-slate-300 '>
-            <div className='flex justify-center pt-10 pb-5 lg:px-5'>
-            <iframe src="https://onedrive.live.com/embed?cid=BBF5E1DD97802BDD&amp;resid=BBF5E1DD97802BDD%21414&amp;authkey=AAKn4868xhnxuhA&amp;em=2&amp;wdAr=1.7777777777777777" width="375px" height="250px" frameborder="0">This is an embedded 
-            <a target="_blank" rel="noreferrer" href="https://office.com">Microsoft Office</a> presentation, powered by 
-            <a target="_blank" rel="noreferrer" href="https://office.com/webapps">Office</a>.</iframe>          
-            </div>
-            <div className='px-10 lg:mt-10 lg:mx-30 lg:justify-center'>
-                <h1 className='text-xl md:text-2xl lg:text-3xl'>Business Pitch</h1>
-                <p  className='text-md py-5 leading-8 text-gray-800 md:text-l lg:text-xl max-w-xl mx-auto dark:text-slate-100'>
-                  Business Pitch Power-Pont Template 
+      <SwiperSlide >
+           <Slide
+                 presentation={
+                  <iframe src="https://onedrive.live.com/embed?resid=BBF5E1DD97802BDD%21418&amp;authkey=%21AJwlPb5FkkuIioI&amp;em=2&amp;wdAr=1.7777777777777777" width="375px" height="250px" frameborder="0">This is an embedded 
+                   <a target="_blank"  rel="noreferrer" href="https://office.com">Microsoft Office</a> presentation, powered by 
+                   <a target="_blank"  rel="noreferrer" href="https://office.com/webapps">Office</a>.</iframe> 
+                 }
+                 Topic="Business Pitch"
+                 detail="
+                    Business Pitch Power-Pont Template 
                    This uses a formal yet engaging style to impress investors and
                     leave them more interested in your business idea than when you started off. 
                     This template is used to bring in the desired investments.
-                </p>
-            </div>
-        </div>
-      </SwiperSlide>
-
-      ...
+                 "
+           />
+      </SwiperSlide>...
     </Swiper></div></div>
   )
 };
