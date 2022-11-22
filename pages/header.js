1129bfa5f808e586ac6893e6c0e74bb22dd3cfe3
jia-ayo <milkjia11@gmail.com>
2022-11-22 04:52:40 +0100

    add meta

--- pages/header.js
+++ pages/header.js
@@ -5,7 +5,11 @@ function Header(){
         <div>
             <Head>
                 <title>Mossiak</title>
-                <meta name="description" content="mossiak powerpoint creating content lead desiginer elvina" />
+                <meta name="description" content="mossiak powerpoint creating content lead desiginer Elvina Ighoraye, we vreate powerpoint and other form of design to please your asthetic" />
+                 <meta name="keywords" content="mosiak design, mosiak designer, Elvina Ighoraye, powerpoint, powerpoint presentation, power point presentation designer, mossiakdesings, mossiak design" />
+                  <meta name="author" content="Mossiak Designs" />
+                  <meta name="author" content="Mossiakdesigns" />
+                  <meta name="author" content="Elvina Ighoraye" />
                 <link rel="icon" href="/favicon.png" />
             </Head>
         </div>
