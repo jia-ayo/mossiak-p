2bc8b357038f3525a66f2027b2d0b9f4a19af9ea
jia-ayo <milkjia11@gmail.com>
2022-11-27 06:56:16 +0100

    contact

--- pages/contact.js
+++ pages/contact.js
@@ -8,7 +8,19 @@ export default function Contact(){
             <Header/>
             <NavBar/>
             <section className='min-h-screen'>
-            
+               <div className=' flex justify-center items-center flex-col'>
+                    <div>
+                       <h1 className='text-center text-2xl font-medium'>Contact Us</h1>  
+                    </div>
+                    <div className='w-2/5'>
+                        <form>
+                            <div className='my-3 mx-auto'>
+                                <input type='text'/>
+                            </div>
+                        </form>
+                    </div>
+  
+               </div>
 
             </section>
             <Footer/>
